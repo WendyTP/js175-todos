813ee100bf7bbfd597f5cba5c20fb0901915ee5c
Wendy <wendy.tsai1988@gmail.com>
2021-08-23 10:30:57 +0200

    set up home page

---
+++ todos.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+// Static data for initial testing (data only lasts during running of app)
+let todoLists = require("./lib/seed-data");
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("lists", {
+    todoLists: todoLists,
+  });
+});
+
+// Listener
+app.listen(port, host, () => {
+  console.log(`Todos is listening on port ${port} of ${host}!`);
+});
