ada93c604f18e65139a2ad6862331edd78bf1789
Wendy <wendy.tsai1988@gmail.com>
2021-08-27 16:11:40 +0200

    client input and error validation

--- todos.js
+++ todos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
@@ -13,11 +14,70 @@ app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false}));
+
+
+// return the list of todo lists sorted by completion status and title.
+const sortTodoLists = function(lists) {
+  let undoneLists = lists.filter(todoList => !todoList.isDone());
+  let doneLists = lists.filter(todoList => todoList.isDone());
+
+  undoneLists.sort(sortByTitle);
+  doneLists.sort(sortByTitle);
+  return [].concat(undoneLists,doneLists);
+}
+
+// Compare todo list titles alphabetically
+const sortByTitle = function(todoListA, todoListB) {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+
 
 app.get("/", (req, res) => {
   res.render("lists", {
-    todoLists: todoLists,
+    todoLists: sortTodoLists(todoLists),
   });
+});
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+app.get("/lists", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/lists", (req, res) => {
+  // validate input
+  let title = req.body.todoListTitle.trim();
+  if (title.length === 0) {
+    res.render("new-list", {
+      errorMessage: "A title was not provided.",
+    });
+  } else if (title.length > 100) {
+    res.render("new-list", {
+      errorMessage: "List title must be between 1 and 100 characters.",
+      todoListTitle: title,
+    });
+  } else if (todoLists.some(list => list.title === title)) {
+    res.render("new-list", {
+      errorMessage: "List title must be unique.",
+      todoListTitle: title,
+    });
+  } else {
+    todoLists.push(new TodoList(title));
+    res.redirect("/lists");
+  }
 });
 
 // Listener
